51e00c683b7fabe6e68817ad71aeeef648d53ae8
SIT)Zhang Kui <zhangkui0926@thundersoft.com>
2018-09-15 22:25:57 +0800

    Create new smart assistant that is a voice control project.

--- src/main/java/com/zhkui/smartassistant/control/testControl.java
+++ src/main/java/com/zhkui/smartassistant/control/testControl.java
@@ -3,16 +3,18 @@ package com.zhkui.smartassistant.control;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testControl
 {
     private Logger mLog = LoggerFactory.getLogger(testControl.class);
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping("/index")
+    public String index(Model model){
         mLog.info("hello world!");
-        return "Hello world!";
+        model.addAttribute("name","zhakui");
+        return "index";
     }
 }
